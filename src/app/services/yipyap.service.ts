8f56f4db52825d482253962d8fec30338f0eb055
Vincent Nguyen <vqn@wifi-roaming-128-4-64-221.host.udel.edu>
2017-12-14 13:59:50 -0500

    added post functionality

--- src/app/services/yipyap.service.ts
+++ src/app/services/yipyap.service.ts
@@ -6,11 +6,22 @@ import 'rxjs/add/operator/map';
 export class YipyapService {
 
   constructor(private http: Http) {
-    console.log('YipYap Service Working...');
    }
 
   getPosts() {
     return this.http.get('http://localhost:3000/api/allposts')
+      .map(res => res.json());
+  }
+  
+  createPost(newPost) {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/api/newpost', JSON.stringify(newPost), {headers: headers})
+      .map(res => res.json());
+  }
+
+  deletePost(id) {
+    return this.http.delete('http://localhost:3000/api/post/' + id + '/delete')
       .map(res => res.json());
   }
 
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -8,8 +8,10 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
   posts: any[];
+  text: string;
 
   constructor(private _yipyapService: YipyapService) {
     this._yipyapService.getPosts()
@@ -17,6 +19,34 @@ export class HomeComponent implements OnInit {
       this.posts = posts;
     });
    }
+
+   createPost(event) {
+    event.preventDefault();
+    var newPost = {
+      _creator: '5a13b3c695fcb47ea5c71308', // Need to change this to take current logged in user's ID. Set as this now for testing
+      createdAt: Date.now(),
+      text: this.text,
+      voteCount: 0,
+      _comments: []
+    }
+    this._yipyapService.createPost(newPost)
+      .subscribe(post => {
+        this.posts.push(post);
+        this.text = '';
+      });
+   }
+
+   deletePost(id) {
+     var posts = this.posts;
+     this._yipyapService.deletePost(id).subscribe(data => {
+         for (var i = 0; i < posts.length; i++) {
+           if (posts[i]._id == id) {
+             posts.splice(i,1);
+           }
+         }
+     });
+   }
+
 
   ngOnInit() {
   }
